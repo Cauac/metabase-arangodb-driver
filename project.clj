a9187c262db22a9e29e73299b06cdbe2ab633b93
anton <anton.garansky@telenordigital.com>
2019-08-25 19:54:58 +0200

    provided profile

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
 
   :min-lein-version "2.5.0"
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.arangodb/arangodb-java-driver "6.0.0"]
-                 [metabase-core "1.0.0-SNAPSHOT" :scope "provided"]]
+  :dependencies [[com.arangodb/arangodb-java-driver "6.0.0"]]
 
-  :profiles {:uberjar {:auto-clean    true
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.0"]
+                                       [metabase-core "1.0.0-SNAPSHOT"]]}
+             :uberjar {:auto-clean    true
                        :aot           :all
                        :javac-options ["-target" "1.8", "-source" "1.8"]
                        :target-path   "target/%s"
