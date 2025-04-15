e9f6abbc7c6baf7d966332a09191f584b511ae47
Anton Garansky <anton.garansky@telenordigital.com>
2025-04-15 12:53:22 +0200

    implement native query execution

--- src/metabase/driver/arangodb.clj
+++ src/metabase/driver/arangodb.clj
@@ -1,51 +1,16 @@
 (ns metabase.driver.arangodb
   (:require [metabase.driver :as driver]
             [metabase.driver.arangodb.connection :as conn]
-    ;[toucan.db :as db]
-            [metabase.util :as metabase-utils]
-            [metabase.models.database :refer [Database]])
-  (:import (com.arangodb.entity ArangoDBVersion CollectionEntity)
-           (com.arangodb.model CollectionsReadOptions)
-           [java.util Map]
-           [com.arangodb ArangoDB$Builder ArangoDatabase ArangoDBException]))
+            [metabase.lib.metadata :as lib.metadata]
+            [metabase.query-processor.store :as qp.store]
+            [metabase.query-processor.reducible :as qp.reducible])
+  (:import [com.arangodb.entity ArangoDBVersion CollectionEntity]
+           [com.arangodb.model CollectionsReadOptions]
+           [com.arangodb ArangoCursor ArangoDB$Builder ArangoDatabase]))
 
 (driver/register! :arangodb)
 
-(def db-connections (atom {}))
-
-(defn- database-details [database-id]
-  (:details
-    (db/select-one [Database :id :engine :details] :id database-id)))
-
-(defn- db-connection [{:keys [host port dbname user password]}]
-  (let [connection (.build (doto (ArangoDB$Builder.)
-                             (.host host port)
-                             (.user user)
-                             (.password password)))]
-    (.db connection dbname)))
-
-(defn- get-connection [database]
-  (let [id (metabase-utils/get-id database)]
-    (or (get @db-connections id)
-        (let [db-conn (-> (database-details id)
-                          (db-connection))]
-          (swap! db-connections assoc id db-conn)
-          db-conn))))
-
-(defn- query-as-single-column [^ArangoDatabase db ^String query]
-  (reduce
-    #(conj %1 {"column" %2}) [] (.query db query nil nil String)))
-
-(defn- query [^ArangoDatabase db ^String query]
-  (try
-    (vec (.query db query nil nil Map))
-    (catch ArangoDBException _
-      (query-as-single-column db query))))
-
-(defmethod driver/supports? [:arangodb :basic-aggregations] [_ _] false)
-
-(defmethod driver/describe-database :arangodb [_ _]
-  {:tables #{}})
+(defmethod driver/database-supports? [:arangodb :basic-aggregations] [_ _ _] false)
 
 ;;; +----------------------------------------------------------------------------------------------------------------+
 ;;; |                                               Connection                                                       |
@@ -66,36 +31,59 @@
   (.shutdown (.arango connection)))
 
 ;;; +----------------------------------------------------------------------------------------------------------------+
-
+;;; |                                               Database sync                                                    |
+;;; +----------------------------------------------------------------------------------------------------------------+
+;;;
+;;; Metabase sync interface: https://github.com/metabase/metabase/blob/master/src/metabase/sync/interface.clj
+;;; Metabase supported types: https://github.com/metabase/metabase/blob/master/src/metabase/types.cljc
+;;;
 (defmethod driver/can-connect? :arangodb [_ db-details]
   (-> (conn/get-db-connection :arangodb :connection-test db-details)
       (.exists)))
 
 (defmethod driver/dbms-version :arangodb [_ db-model]
-  (let [db (conn/get-db-connection :arangodb (:id db-model) (:details db-model))
+  (let [db (conn/get-db-connection db-model)
         ^ArangoDBVersion server-info (.getVersion db)]
     {:version (.getVersion server-info)
      :flavour (.toString (.getLicense server-info))}))
 
 (defn- collection->table-description [^CollectionEntity col]
-  {:schema nil
-   :name (.getName col)
-   :type "TABLE"})
+  {:name (.getName col)
+   :schema nil})
 
 (defmethod driver/describe-database :arangodb [_ db-model]
-  (let [db (conn/get-db-connection :arangodb (:id db-model) (:details db-model))
+  (let [db (conn/get-db-connection db-model)
         filter (-> (CollectionsReadOptions.)
                    (.excludeSystem true))
         tables (->> (.getCollections db filter)
                     (map collection->table-description))]
     {:tables (set tables)}))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmethod driver/execute-query :arangodb [_ query-params]
-  (let [db-connection (get-connection (:database query-params))
-        query-str (get-in query-params [:native :query])
-        results (query db-connection query-str)
-        columns (map name (keys (first results)))
-        rows (map vals results)]
-    {:columns columns
-     :rows    rows}))
+;;; +----------------------------------------------------------------------------------------------------------------+
+;;; |                                               Query execution                                                  |
+;;; +----------------------------------------------------------------------------------------------------------------+
+
+(defn- result-metadata [first-row]
+  {:cols (mapv (fn [col-name] {:name col-name}) (.keySet first-row))})
+
+(defn- reducible-rows [^ArangoCursor cursor first-row]
+  (let [has-returned-first-row? (volatile! false)
+        restored-iterator (fn []
+                            (if-not @has-returned-first-row?
+                              (do (vreset! has-returned-first-row? true)
+                                  (vals first-row))
+                              (when (.hasNext cursor)
+                                (vals (.next cursor)))))]
+    (qp.reducible/reducible-rows restored-iterator)))
+
+(defn- reduce-results [^ArangoCursor cursor respond]
+  (if-let [first-row (when (.hasNext cursor) (.next cursor))]
+    (respond (result-metadata first-row) (reducible-rows cursor first-row))
+    (respond {} [])))
+
+(defmethod driver/execute-reducible-query :arangodb [_ query _context respond]
+  (let [query-str (get-in query [:native :query])
+        db-model (lib.metadata/database (qp.store/metadata-provider))
+        ^ArangoDatabase db (conn/get-db-connection db-model)]
+    (with-open [^ArangoCursor cursor (.query db query-str nil)]
+      (reduce-results cursor respond))))
